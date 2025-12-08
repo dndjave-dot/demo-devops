bc6f16579c3a7c8770351d3751a133e8ecf1cb60
dndjave-dot <dndjave@gmail.com>
2025-12-08 01:04:33 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,7 @@ resource "docker_image" "nginx" {
 
 resource "docker_container" "web" {
   name  = "nginx-container"
-  image = docker_image.nginx.latest
+  image = docker_image.nginx.image_id
 
   ports {
     internal = 80
